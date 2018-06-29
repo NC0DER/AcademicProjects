212c2d66c829af03b8e15808ca6bae507d384abc
Unknown <dynarithm@gmail.com>
2018-06-29 13:43:50 +0300

    Added Key Value storage struct

--- Computer Networks/serv1.c
+++ Computer Networks/serv1.c
@@ -12,3 +12,15 @@
 #include <arpa/inet.h>
 #include "keyvalue.h"
 
+#define BACKLOG 5
+#define BUFFERSIZE 2055
+
+struct kvstore_node{
+    char key[1025];
+    char value[1025];
+    struct kvstore_node *next_node;
+};
+
+struct kvstore_node* kvstore;
+struct kvstore_node** ptr; //Pointer to reference the store
+
