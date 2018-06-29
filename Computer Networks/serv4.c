d18ca95fb7ce88e118ac1dba36ac00f7ddd556c8
Unknown <dynarithm@gmail.com>
2018-06-29 14:52:28 +0300

    Added Signal Handler
    and pthread mutex initialization.

--- Computer Networks/serv4.c
+++ Computer Networks/serv4.c
@@ -14,3 +14,27 @@
 #include <arpa/inet.h>
 #include "keyvalue.h"
 
+#define BACKLOG 5
+#define BUFFERSIZE 2055
+
+volatile sig_atomic_t uninterrupted = 1;
+void handle_cleanup(int signal_flag)
+{
+    uninterrupted = 0;
+}
+
+struct kvstore_node{
+    char key[1025];
+    char value[1025];
+    struct kvstore_node *next_node;
+};
+
+struct kvstore_node* kvstore;
+struct kvstore_node** ptr; //Pointer to reference the store
+pthread_mutex_t data_mutex = PTHREAD_MUTEX_INITIALIZER;
+/*
+      The same mutex is used for both put() and get()
+      In order to mutually exclude any thread, from calling put()/get() concurrently
+      while another thread, also calls put()/get(), thus avoiding the race condition
+      on this globally shared memory segment (key value store)
+*/
