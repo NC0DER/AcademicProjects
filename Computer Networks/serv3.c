3932e109c94e265db46494054c718960581153a9
Unknown <dynarithm@gmail.com>
2018-06-29 14:38:07 +0300

    Added Server Headers

---
+++ Computer Networks/serv3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>  //System V semaphore
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include "keyvalue.h"
+
