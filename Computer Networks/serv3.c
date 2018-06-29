e2445212c65667ab36cfc73f6b771290b569fe58
Unknown <dynarithm@gmail.com>
2018-06-29 14:41:55 +0300

    Started main and added semaphore initialization

--- Computer Networks/serv3.c
+++ Computer Networks/serv3.c
@@ -95,3 +95,42 @@ ssize_t writen(int fd, const void *vptr, size_t n){ //Wrapper function from Stev
     return n;
 }
 
+int main(int argc, char *argv[]) {
+    int i = 1, j = 0, numberof_procs = 0, sockfd = 0, new_sockfd = 0;
+    ssize_t numberof_bytes = 0;
+    size_t size = 0;
+    pid_t *pid_table = NULL, pid;
+    char *port_number = NULL;
+    char *buf_token = NULL;
+    char *returned = NULL;
+    char *buffer = malloc(sizeof(*buffer) * BUFFERSIZE);
+    struct sigaction sigint_action, sigchld_action;
+    struct addrinfo address_info, *server_info, *pntr;
+    struct sockaddr_storage client_address; // connector's address information
+    socklen_t address_size;
+
+    up.sem_num = 0; //only one semaphore used
+    up.sem_op = 1; //semval +1 => semaphore up
+    up.sem_flg = 0; //no special flags
+
+    down.sem_num = 0; //only one semaphore used
+    down.sem_op = -1; //semval -1 => semaphore down
+    down.sem_flg = 0; //no special flags
+
+    /*
+      The same semaphore is used for both put() and get()
+      In order to mutually exclude any child process, from calling put()/get() concurrently
+      while another child process, also calls put()/get(), thus avoiding the race condition
+      on this shared memory segment (key value kvstore)
+   */
+    if (argc != 3) {
+        perror("Syntax: ./server <port_number> <number_of_processes>");
+        free(buffer);
+        exit(EXIT_FAILURE);
+    }
+    port_number = strdup(argv[1]);
+    numberof_procs = atoi(argv[2]);
+    if (numberof_procs <= 0) {
+        perror("Syntax: number_of_processes should be a positive non-zero integer");
+        exit(EXIT_FAILURE);
+    }
