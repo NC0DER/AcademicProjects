dd53e91daa0802c708cb6ee4e2c281ce15fb9e3a
Unknown <dynarithm@gmail.com>
2018-06-30 16:55:54 +0300

    Completed getName

--- Object Oriented Programming/ship.cpp
+++ Object Oriented Programming/ship.cpp
@@ -3,3 +3,10 @@
  int Ship::id = 0;//Total Number of ships
  int Sea::count_ref = 0;//Number of Sea tiles destroyed
 
+std::string Ship::getName()const
+{
+    try{
+        return name;
+    }
+    catch (std::bad_alloc& ba){}
+}
