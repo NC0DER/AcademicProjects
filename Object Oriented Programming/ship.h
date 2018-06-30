1d99278b0fd854fd3a3a09a2eaec6ec023532345
Unknown <dynarithm@gmail.com>
2018-06-30 16:40:20 +0300

    Added Headers on ship.h
    and include guard.

---
+++ Object Oriented Programming/ship.h
@@ -0,0 +1,13 @@
+#ifndef SHIP_H
+#define SHIP_H
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <ctime>
+#include <new>
+#define CUSTOM_HP 500
+#define START_GOLD 100
+#define _SIZE_ 4 //Recommended Size >= 4
+
