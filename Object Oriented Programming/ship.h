3c15aa61aa7c0b6e4e9d0f00ddeb386df7eb5a61
Unknown <dynarithm@gmail.com>
2018-06-30 16:43:08 +0300

    Started Class Ship

--- Object Oriented Programming/ship.h
+++ Object Oriented Programming/ship.h
@@ -11,3 +11,40 @@
 #define START_GOLD 100
 #define _SIZE_ 4 //Recommended Size >= 4
 
+class Sea;
+class Ship{
+public: static int id; //Number of all Ships created since start
+
+protected:
+    std::string name;
+    std::string type;
+    int HP;
+    int maxHP;
+private:
+    int gold, AP;//All accumulative stats are zero upon creation except AP,Gold
+    int	TotalMov, TotalGold, TotalDmgTaken, TotalDmgDealt;
+    int x,y; //Coordinates of ship
+    int curr_id;
+public:
+    Ship(std::string _name = "Ship"){
+        gold = START_GOLD;
+        TotalMov = 0;
+        TotalDmgDealt = 0;
+        x = y = 0;
+        TotalGold = START_GOLD;
+        TotalDmgTaken = 0;
+        AP = 2;
+        std::ostringstream tmp;
+        std::string temp;
+        tmp << _name << ++id;
+        name = tmp.str();
+        curr_id = id;
+    }
+    virtual void func(Sea** Map)=0;
+    virtual void mov(Sea** Map)=0;
+
+    ~Ship(){
+        std::cout << name << " (" << type << " type) has been destroyed!!" << std::endl << std::endl;
+    }
+};
+};
