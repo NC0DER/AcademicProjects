682a6494ab738f215d462426ee76f733884c1211
Unknown <dynarithm@gmail.com>
2018-06-30 19:21:42 +0300

    Finished Medic.cpp
    by completing Medic Ship's move function.

--- Object Oriented Programming/medic.cpp
+++ Object Oriented Programming/medic.cpp
@@ -68,3 +68,54 @@ void Medic::mov(Sea** Map)
 		}
 	}
 }
+void Medic::func(Sea** Map)
+{
+	int x_ref = getX();
+	int y_ref = getY();
+
+	int posX = 0;
+	int posY = 0;
+
+	if (Map[x_ref][y_ref].getLoot() > 0)
+	{
+		setGold(Map[x_ref][y_ref].getLoot());
+		std::cout << name << " has discovered a treasure box (+" << Map[x_ref][y_ref].getLoot() << " gold)" << std::endl;
+		setTotalGold(Map[x_ref][y_ref].getLoot());
+		Map[x_ref][y_ref].setLoot(0);
+	}
+	if (Map[x_ref][y_ref].getWeather() >= 8)
+	{
+		setHP(-CUSTOM_HP);
+		std::cout << name << " has taken " << CUSTOM_HP << " damage from humongous waves inside a ferocious storm!" << std::endl;
+		setTotalDmgTaken(CUSTOM_HP);
+	}
+	for (int i = -1; i <= 1; i++)
+	{
+		for (int j = -1; j <= 1; j++)
+		{
+			posX = mod(x_ref + i,_SIZE_);
+			posY = mod(y_ref + j, _SIZE_);
+			if (Map[posX][posY].gethasShip() == true)
+			{
+				if (((posX != x_ref) || (posY != y_ref)))
+				{
+					int HP_ref = Map[posX][posY].place->getHP();
+					int maxHP_ref = Map[posX][posY].place->getmaxHP();
+					int gold_ref = Map[posX][posY].place->getGold();
+					if ((HP_ref < maxHP_ref) && (HP_ref >= 0) && (gold_ref >= (static_cast<int>(0.1 * START_GOLD))))
+					{
+						Map[posX][posY].place->setHP(static_cast<int>(0.5 * CUSTOM_HP));
+						Map[posX][posY].place->setGold(static_cast<int>(-0.1 * START_GOLD));
+						setGold(static_cast<int>(0.1 * START_GOLD));
+						setTotalGold(static_cast<int>(0.1 * START_GOLD));
+						std::cout << name << " repaired " << Map[posX][posY].place->getName() << " (" << posX << "," << posY << ") for " << static_cast<int>(0.5 * CUSTOM_HP) << " HP "
+							 << "while redeeming " << static_cast<int>(0.1 * START_GOLD) <<" gold "<< std::endl;
+						goto Repair_Once; //Repairs only one ship then exits nested search loop
+					}
+				}
+			 }
+		}
+	}
+	Repair_Once:
+	std::cout << std::endl;
+}
