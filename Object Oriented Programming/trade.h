54f4d609ff126eba2f2c55bfe47025ab23e6c65e
Unknown <dynarithm@gmail.com>
2018-06-30 18:59:12 +0300

    Finished Trade.h

---
+++ Object Oriented Programming/trade.h
@@ -0,0 +1,24 @@
+#ifndef TRADE_H
+#define TRADE_H
+#include "ship.h"
+class Trade : public Ship
+{
+    public:
+        Trade():Ship("Trade Ship")
+        {
+            maxHP = 3 * CUSTOM_HP;
+            HP = 3 * CUSTOM_HP;
+            type = "Trade";
+        }
+        Trade(std::string name):Ship(name)
+        {
+            this->name = name;
+            maxHP = 3 * CUSTOM_HP;
+            HP = 3 * CUSTOM_HP;
+            type = "Trade";
+        }
+    protected:
+		void func(Sea** Map);
+		void mov(Sea** Map);
+};
+#endif
