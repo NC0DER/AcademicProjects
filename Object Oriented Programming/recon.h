1e6097ac06882caa9997a7c0565c6251bfe3603c
Unknown <dynarithm@gmail.com>
2018-06-30 18:36:37 +0300

    Completed Recon.h

---
+++ Object Oriented Programming/recon.h
@@ -0,0 +1,25 @@
+#ifndef RECON_H
+#define RECON_H
+#include "ship.h"
+class Recon : public Ship
+{
+    public:
+        Recon():Ship("Recon Ship")
+        {
+            maxHP = CUSTOM_HP;
+            HP = CUSTOM_HP;
+            type = "Recon";
+        }
+        Recon(std::string name)
+        {
+            this->name = name;
+            maxHP = CUSTOM_HP;
+            HP = CUSTOM_HP;
+            type = "Recon";
+        }
+    protected:
+		void func(Sea** Map);
+		void mov(Sea** Map);
+
+};
+#endif
