770b43e4f8ae0f8cb50f963705d7c4f19c88ff54
Unknown <dynarithm@gmail.com>
2018-07-02 16:44:39 +0300

    Added necessary Headers

---
+++ Simple Linux Shell/myshell.c
@@ -0,0 +1,13 @@
+﻿#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#define MAX_SIZE 800
+
+struct command {
+    const char ** argv;
+};
+
