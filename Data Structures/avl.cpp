e27ee9a281001d3f6c7f937b4b2116c46297af46
Unknown <dynarithm@gmail.com>
2018-07-02 20:41:40 +0300

    Completed AVL Tree fix height  method

--- Data Structures/avl.cpp
+++ Data Structures/avl.cpp
@@ -11,3 +11,8 @@ int AVLTree::hb(AVLNode* ptr) //Height Balance Factor
     return height(ptr->right) - height(ptr->left);
 }
 
+void AVLTree::fixheight(AVLNode* ptr)
+{
+    ptr->height = ((height(ptr->left) > height(ptr->right)) ? height(ptr->left) : height(ptr->right)) + 1;
+}
+
