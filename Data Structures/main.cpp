d61b4b68368ae2b4d4d3749bf05c289d294f0daf
Unknown <dynarithm@gmail.com>
2018-07-03 19:16:57 +0300

    Started Main function

--- Data Structures/main.cpp
+++ Data Structures/main.cpp
@@ -32,3 +32,25 @@ bool check_flags(std::ifstream * );
 int BinarySearch(Company * , int, int, int);
 int InterpolationSearch(Company * , int, int, int);
 
+int max_id = 0;
+int main(int argc, char * argv[]) {
+    bool exit_flag = false;
+    bool file_exists = false;
+
+    std::string filename = "";
+    AVLTree tree;
+    AVLNode * root = NULL;
+
+    Companies log;
+    log.numberOfCompanies = 0;
+    log.arr = NULL;
+
+    std::cout << "Exiting...." << std::endl;
+    if (log.arr) //free arr from previous paths of executions
+    {
+        free(log.arr -> employees);
+        free(log.arr);
+    }
+    delete root;
+    return 0;
+}
