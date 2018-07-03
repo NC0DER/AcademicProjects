297bd86febf3aa44e65d189f145b017b763f0a76
Unknown <dynarithm@gmail.com>
2018-07-03 19:08:41 +0300

    Declared necessary functions

--- Data Structures/main.cpp
+++ Data Structures/main.cpp
@@ -19,3 +19,16 @@
     #include <windows.h>
 #endif
 
+void clear_screen();
+void mpause();
+int inputInt();
+int str2int(std::string);
+int copy2cstr(char * , const char * , int); //Buf_Size < strlen(destination) or else BUFFER OVERFLOW!!
+void printChar(char[], int);
+void printComp(Company * );
+void swapComp(Company * , Company * );
+void insort(Company * , int);
+bool check_flags(std::ifstream * );
+int BinarySearch(Company * , int, int, int);
+int InterpolationSearch(Company * , int, int, int);
+
