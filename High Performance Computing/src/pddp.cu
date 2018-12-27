1990009dda7fefdbeb21bcc6dd5c26304a2b13ec
Nikos Giarelis <dynarithm@gmail.com>
2018-12-27 15:09:44 +0200

    Implemented initialize_vector wrapper & kernel function

--- High Performance Computing/src/pddp.cu
+++ High Performance Computing/src/pddp.cu
@@ -1,1 +1,10 @@
 #include "../include/pddp.cuh"
+
+__global__ void initialize_vector_kernel(matrix x, double value)
+{
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (index < x.rows) {
+        x.data[index] = value;
+    }
+}
--- High Performance Computing/src/wrappers.cu
+++ High Performance Computing/src/wrappers.cu
@@ -1,2 +1,19 @@
 #include <iostream>
 #include "../include/wrappers.cuh"
+
+void initialize_vector(matrix x, double value, unsigned int blockSizeX)
+{
+    cudaError_t cudaStatus;
+    // Define grid dimensions
+    unsigned int numBlocksX = (x.rows - 1) / blockSizeX + 1;
+    dim3 dimGrid(numBlocksX, 1, 1);
+    dim3 dimBlock(blockSizeX, 1, 1);
+    initialize_vector_kernel << <dimGrid, dimBlock >> > (x, value);
+
+    // cudaDeviceSynchronize waits for the kernel to finish.
+    // While returing any errors that happended during the kernel launch.
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        std::cerr << "cudaDeviceSynchronize returned error code " << cudaStatus << " after launching initialize_vector Kernel!" << std::endl;
+    }
+}
