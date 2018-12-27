d8821a2f36e8bfb2711cb14f500d7ecde41ca399
Nikos Giarelis <dynarithm@gmail.com>
2018-12-27 13:30:53 +0200

    Added headers in main.cu

---
+++ High Performance Computing/src/main.cu
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "../include/matrix.h"
+#include "../include/wrappers.cuh"
+
+#define debug 1
+
+int main(int argc, char *argv[]) {
+    return EXIT_SUCCESS;
+}
