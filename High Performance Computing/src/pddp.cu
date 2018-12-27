0a1198db90991b7b08df57c66919c9881260ebcc
Nikos Giarelis <dynarithm@gmail.com>
2018-12-27 15:27:33 +0200

    Implemented initialize_w wrapper & kernel function

--- High Performance Computing/src/pddp.cu
+++ High Performance Computing/src/pddp.cu
@@ -1,5 +1,19 @@
 #include "../include/pddp.cuh"
 
+__global__ void initialize_w_kernel(matrix M, matrix w)
+{
+    int row = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < M.rows) {
+        double sum = 0;
+        for (size_t col = 0; col < M.cols; ++col) {
+            sum += M.data[idx(row, M.cols, col)];
+        }
+        w.data[row] = sum / M.cols;
+    }
+
+}
+
 __global__ void initialize_vector_kernel(matrix x, double value)
 {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
--- High Performance Computing/src/wrappers.cu
+++ High Performance Computing/src/wrappers.cu
@@ -17,3 +17,25 @@ void initialize_vector(matrix x, double value, unsigned int blockSizeX)
         std::cerr << "cudaDeviceSynchronize returned error code " << cudaStatus << " after launching initialize_vector Kernel!" << std::endl;
     }
 }
+
+void initialize_w(matrix M, matrix w, unsigned int blockSizeX)
+{
+    cudaError_t cudaStatus;
+    // Define grid dimensions
+    dim3 dimBlock;
+    dim3 dimGrid;
+    dimBlock.x = blockSizeX;
+    dimBlock.y = 1;
+    dimBlock.z = 1;
+    dimGrid.x = (M.rows - 1) / dimBlock.x + 1;
+    dimGrid.y = 1;
+    dimGrid.z = 1;
+    initialize_w_kernel << <dimGrid, dimBlock >> > (M, w);
+
+    // cudaDeviceSynchronize waits for the kernel to finish.
+    // While returing any errors that happended during the kernel launch.
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        std::cerr << "cudaDeviceSynchronize returned error code " << cudaStatus << " after launching initialize_w Kernel!" << std::endl;
+    }
+}
